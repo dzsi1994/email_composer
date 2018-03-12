631d579d604712dae345b19ac198da15792c29aa
dzsi1994 <nagyg@gain.nyme.hu>
2018-03-12 22:07:19 +0100

    display navbar only in email-form component. Add some style adjustments

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import {
+  Location,
+  LocationStrategy,
+  PathLocationStrategy
+} from '@angular/common';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  preview: boolean;
+  location: Location;
+  constructor(private router: Router, location: Location) {
+    this.location = location;
+   }
+  
+  ngOnInit() {}
+  public isHidden() {
+    const list = ['/preview-email'];
+    const route = this.location.path();
 
-  constructor() { }
-
-  ngOnInit() {
+    return list.indexOf(route) > -1;
   }
 
 }
